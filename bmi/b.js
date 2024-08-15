e149fe89f1f8215418ba780182704fc1819aff90
daniyaljumshaid <daniyaljumshaid74@gmail.com>
2024-08-15 13:00:48 +0100

    Add files via upload

---
+++ bmi/b.js
@@ -0,0 +1,25 @@
+// let h=document.querySelector("#box1");
+// let w=document.querySelector("#box2");
+// let button=document.querySelector("#calculateButton");
+// let resultDiv = document.querySelector("#result");
+
+// button.addEventListener("click",(e)=>{
+//     let height =h.value;
+//     let weight =w.value;
+// let bmi = weight / (height * height);
+// var m;
+            
+//             if(bmi<18.5){
+//                 m="you are underweight.";
+//             }
+//             else if(bmi>=18.5 && bmi<24.5){
+//                 m="You are in a healthy range.";
+//             }
+//             else if(bmi>=25.0 && bmi<29.9){
+//                 m="You are overweight.";
+//             }
+//             else{
+//                 m="You are obese.";
+//             }  
+//             resultDiv.textContent = `Your BMI is: ${bmi} and ${m}`;
+//         })
